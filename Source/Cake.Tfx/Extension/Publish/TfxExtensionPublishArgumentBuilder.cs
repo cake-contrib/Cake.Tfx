c7e97d85adc7a380e3cd471fbc2ba060be1eb823
Gary Ewan Park <gep13@gep13.co.uk>
2016-09-07 23:10:38 +0100

    (build) Added missing files

---
+++ Source/Cake.Tfx/Extension/Publish/TfxExtensionPublishArgumentBuilder.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using Cake.Core;
+using Cake.Core.IO;
+
+namespace Cake.Tfx.Extension.Publish
+{
+    /// <summary>
+    /// The Argument Builder for the Publish Extension method of the Tfx CLI.
+    /// </summary>
+    internal sealed class TfxExtensionPublishArgumentBuilder : ITfxArgumentBuilder
+    {
+        private readonly ICakeEnvironment _environment;
+        private readonly TfxExtensionPublishSettings _settings;
+        private readonly FilePath _vsixFilePath;
+        private readonly ICollection<string> _shareWith;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TfxExtensionPublishArgumentBuilder"/> class.
+        /// </summary>
+        /// <param name="environment">The environment.</param>
+        /// <param name="vsixFilePath">The path to the VSIX.</param>
+        /// <param name="shareWith">The accounts to publish the extension to.</param>
+        /// <param name="settings">The settings.</param>
+        public TfxExtensionPublishArgumentBuilder(ICakeEnvironment environment, FilePath vsixFilePath, ICollection<string> shareWith, TfxExtensionPublishSettings settings)
+        {
+            _environment = environment;
+            _vsixFilePath = vsixFilePath;
+            _shareWith = shareWith;
+            _settings = settings;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TfxExtensionPublishArgumentBuilder"/> class.
+        /// </summary>
+        /// <param name="environment">The environment.</param>
+        /// <param name="settings">The settings.</param>
+        public TfxExtensionPublishArgumentBuilder(ICakeEnvironment environment, TfxExtensionPublishSettings settings)
+        {
+            _environment = environment;
+            _settings = settings;
+        }
+
+        /// <summary>
+        /// Adds the arguments to the specified argument builder.
+        /// </summary>
+        /// <returns>A populated <see cref="ProcessArgumentBuilder"/>.</returns>
+        public ProcessArgumentBuilder GetArguments()
+        {
+            var builder = new ProcessArgumentBuilder();
+
+            builder.Append("extension publish");
+
+            if (_vsixFilePath != null)
+            {
+                builder.Append("--vsix");
+                builder.AppendQuoted(_vsixFilePath.MakeAbsolute(_environment).FullPath);
+            }
+
+            if (_shareWith != null && _shareWith.Count != 0)
+            {
+                builder.Append("--share-with");
+
+                foreach (var account in _shareWith)
+                {
+                    builder.AppendQuoted(account);
+                }
+            }
+
+            TfxArgumentBuilder.GetServerArguments(builder, _settings);
+            TfxArgumentBuilder.GetCreatePublishArgument(builder, _environment, _settings);
+            TfxArgumentBuilder.GetCommonArguments(builder, _settings);
+
+            return builder;
+        }
+    }
+}
---
+++ Source/Cake.Tfx/Extension/Publish/TfxExtensionPublishRunner.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Cake.Core;
+using Cake.Core.IO;
+using Cake.Core.Tooling;
+
+namespace Cake.Tfx.Extension.Publish
+{
+    /// <summary>
+    /// The Tfx Publish Runner used to publish extensions.
+    /// </summary>
+    public sealed class TfxExtensionPublishRunner : TfxTool<TfxExtensionPublishSettings>
+    {
+        private readonly ICakeEnvironment _environment;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TfxExtensionPublishRunner"/> class.
+        /// </summary>
+        /// <param name="fileSystem">The file system.</param>
+        /// <param name="environment">The environment.</param>
+        /// <param name="processRunner">The process runner.</param>
+        /// <param name="tools">The tool locator.</param>
+        public TfxExtensionPublishRunner(
+            IFileSystem fileSystem,
+            ICakeEnvironment environment,
+            IProcessRunner processRunner,
+            IToolLocator tools)
+            : base(fileSystem, environment, processRunner, tools)
+        {
+            _environment = environment;
+        }
+
+        /// <summary>
+        /// Installs an extension using the specified settings.
+        /// </summary>
+        /// <param name="vsixFilePath">The path to the VSIX.</param>
+        /// <param name="shareWith">The accounts to publish the extension to.</param>
+        /// <param name="settings">The settings.</param>
+        public void Publish(FilePath vsixFilePath, ICollection<string> shareWith, TfxExtensionPublishSettings settings)
+        {
+            if (vsixFilePath == null)
+            {
+                throw new ArgumentNullException(nameof(vsixFilePath));
+            }
+
+            if (shareWith == null || shareWith.Count == 0)
+            {
+                throw new ArgumentNullException(nameof(shareWith));
+            }
+
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            RunTfx(settings, new TfxExtensionPublishArgumentBuilder(_environment, vsixFilePath, shareWith, settings));
+        }
+
+        /// <summary>
+        /// Installs an extension using the specified settings.
+        /// </summary>
+        /// <param name="settings">The settings.</param>
+        public void Publish(TfxExtensionPublishSettings settings)
+        {
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            RunTfx(settings, new TfxExtensionPublishArgumentBuilder(_environment, settings));
+        }
+    }
+}
